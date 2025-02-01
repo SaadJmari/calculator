2f7828c4d0c278015443c79bee81fefb9f95f12a
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-02-01 15:08:27 +0100

    Handle storing numbers and operations

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const operationsContainer = document.querySelector(".operators")
 const numbersBtn = ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0", "="]
 const operationsBtn = ["+", "-", "*", "/"]
 const display = document.querySelector(".display")
-let firstNum, secondNum
+const btns = document.querySelectorAll("button")
+let firstNum, secondNum, operator, result
+
 
 
 //Creating the calculator's buttons
@@ -22,6 +24,25 @@ operationsBtn.forEach(op => {
     operationsContainer.appendChild(buttons)
 })
 
+//Operations
+const sum = function (a, b) {
+    return a + b
+}
+
+const substraction = function (a, b) {
+    return a - b
+}
+
+const multiply = function (a, b) {
+    return a * b
+}
+
+const division = function (a, b) {
+    if (b == 0) { return "error!" }
+    else { return a / b }
+}
+
+//Hundle numbers display
 numbersContainer.addEventListener('click', (event) => {
     let target = event.target
     if (target.classList == "number" && display.textContent.length < 15) {
@@ -32,3 +53,46 @@ numbersContainer.addEventListener('click', (event) => {
     }
 })
 
+//Storing numbers and operators
+operationsContainer.addEventListener('click', (event) => {
+    let target = event.target
+    if (target.classList == "operations") {
+        if (firstNum != undefined) {
+            secondNum = Number(display.textContent)
+        }
+        firstNum = Number(display.textContent)
+        operator = target.textContent
+        display.textContent = ""
+    }
+})
+
+btns.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const target = event.target
+
+        if (target.textContent == "=") {
+            if (firstNum != undefined && display.textContent != "") {
+                secondNum = Number(display.textContent)
+
+                switch (operator) {
+                    case '+':
+                        result = sum(firstNum, secondNum)
+                        break;
+                    case '-':
+                        result = substraction(firstNum, secondNum)
+                        break;
+                    case '*':
+                        result = multiply(firstNum, secondNum)
+                        break;
+                    case '/':
+                        result = division(firstNum, secondNum)
+                        break;
+                }
+                display.textContent = result
+                firstNum = undefined
+                secondNum = undefined
+            }
+        }
+    })
+
+})
